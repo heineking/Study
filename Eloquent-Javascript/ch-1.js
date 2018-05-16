a2590e4c7b05a57a9f674cf8bbcf5050d3a45b82
Emil Heineking <ebheineking@gmail.com>
2018-05-15 23:15:31 -0400

    finished ch-2 eloquent js

--- Eloquent-Javascript/ch-1.js
+++ Eloquent-Javascript/ch-1.js
@@ -1,27 +1,37 @@
 /*
-  Exercise 1. Write a function that outputs the following:
 
-  #
-  ##
-  ###
-  ####
-  #####
-  ######
-
-  Where the last row is the baseLength of the triangle
 */
-{
-  console.log("-- start ex 1 --");
+const answers = {};
 
+function exercise(answer) {
+  console.log(answer.question);
+  answer();
+}
+
+/* Exercise 1 ================================================================*/
+
+exerciseOne.question = `
+Exercise 1. Write a function that outputs the following:
+
+#
+##
+###
+####
+#####
+######
+
+Where the last row is the baseLength of the triangle
+`;
+
+function exerciseOne() {
   const range = function*(start, end) {
     while(start <= end)
       yield start++;
   }
 
   const repeat = function*(action, times) {
-    while (times-- > 0) {
+    while (times-- > 0)
       yield action();
-    }
   };
 
   const drawTriangle = (baseLength) => [...range(1, baseLength)]
@@ -30,12 +40,22 @@
     .forEach(row => console.log(row));
 
   drawTriangle(10);
-
-  console.log("-- done ex 1 --");
 }
 
-{
-  console.log("-- start ex 2 --");
+// exercise(exerciseOne);
+
+/* Exercise 2 ================================================================*/
+
+exerciseTwo.question = `
+Write a program that uses console.log to print all the numbers from 1 to 100, 
+with two exceptions. For numbers divisible by 3, print "Fizz" instead of the 
+number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+
+When you have that working, modify your program to print "FizzBuzz" for numbers
+that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers 
+divisible by only one of those).
+`
+function exerciseTwo() {
  
   // this is an overly complicated solution to the exercise. An easier way would
   // be to just use imperative for-loop and test each input and render the output
@@ -94,6 +114,47 @@
   );
 
   [...range(1, 100)].forEach(actions);
-
-  console.log("-- end ex 2 --");
 }
+
+//exercise(exerciseTwo);
+
+function exercise3() {
+
+  function *range(start, end) {
+    while (start <= end)
+      yield start++;
+  }
+
+  function *repeat(action, times) {
+    while (times-- > 0)
+      yield action();
+  }
+
+  function curryN(f, arity = f.length, received = []) {
+    return (...args) => {
+      const combined = [...received, ...args];
+      const argsLeft = arity - combined.length;
+      return argsLeft > 0
+        ? curryN(f, argsLeft, combined)
+        : f.apply(null, combined);
+    }
+  }
+
+  const row = curryN((unit, length) => [...repeat(() => unit, length/2)].join(""));
+  const oddRow = row(" #");
+  const evenRow = row("# ");
+
+  const chessboard = (size) => [...range(1, size)].reduce((board, row) =>
+    board += `${row % 2 === 1 ? oddRow(size) : evenRow(size)}\n`, "");
+
+  console.log(chessboard(8));
+  console.log(chessboard(14));
+}
+
+exercise3.question = `
+Write a program that creates a string that represents an 8×8 grid, using newline
+characters to separate lines. At each position of the grid there is either a space 
+or a "#" character. The characters should form a chessboard.
+`;
+
+exercise(exercise3);
