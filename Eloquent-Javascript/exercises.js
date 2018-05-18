dee1449cbb21924ad8c80b07c826aa963966bb09
Emil Heineking <ebheineking@gmail.com>
2018-05-17 21:12:42 -0400

    more exercises

--- Eloquent-Javascript/exercises.js
+++ Eloquent-Javascript/exercises.js
@@ -214,3 +214,46 @@ function exercise5() {
 }
 
 /* Exercise 6 ================================================================*/
+
+exercise6.question = `
+Write a range function that takes two arguments, start and end, and returns an 
+array containing all the numbers from start up to (and including) end.
+
+Next, write a sum function that takes an array of numbers and returns the sum of 
+these numbers. Run the example program and see whether it does indeed return 55.
+
+As a bonus assignment, modify your range function to take an optional third 
+argument that indicates the “step” value used when building the array. If no 
+step is given, the elements go up by increments of one, corresponding to the 
+old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. 
+Make sure it also works with negative step values so that range(5, 2, -1) 
+produces [5, 4, 3, 2].
+`;
+
+function exercise6() {
+
+  // non-generator example of range
+  
+  function range(start, stop, step = 1) {
+    const len = Math.abs(start - stop);
+    const xs = [];
+    
+    for(let i = 0; i <= len; ++i)
+      if (i % step === 0) {
+        xs.push(start);
+        start += step;
+      }
+
+    return xs;
+  }
+
+  console.log(JSON.stringify(range(1, 10)));
+  console.log(JSON.stringify(range(1, 10, 2)));
+  console.log(JSON.stringify(range(5, 2, -1)));
+
+  const sum = xs => xs.reduce((sum, x) => sum + x, 0);
+
+  console.log(sum(range(1, 10)));
+}
+
+exercise(exercise6);
