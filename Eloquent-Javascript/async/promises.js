73416cbd29ea06daeb7060b599d3747b1fe5173d
Emil Heineking <ebheineking@gmail.com>
2018-07-06 07:11:40 -0400

    refactored promises timeout example to be more functional

--- Eloquent-Javascript/async/promises.js
+++ Eloquent-Javascript/async/promises.js
@@ -1,34 +1,40 @@
 class Timeout extends Error { };
 
-const timeout = (ms) => new Promise((_, reject) => {
-  setTimeout(() => reject(new Timeout("Timed out")), ms);
-});
-
-const reattempt = (request, ms = 250, n = 1, max = 3) => {
-  return new Promise((resolve, reject) => {
-    let done = false;
-    const requests = [request(), timeout(max*ms)];
-    for (let i = n; i <= max; ++i) {
-      requests.push(
-        timeout(i*ms)
-          .catch(err => {
-            if (!done)
-              return request();
-          })
-      );
-    }
-    Promise
-      .race(requests)
-      .then(result => {
-        done = true;
-        resolve(result); 
-      })
-      .catch(reject);
-  });
+const curry = (f, arity = f.length, received = []) => {
+ return (...args) => {
+    const combined = [...received, ...args];
+    const left = arity - combined.length;
+    return left > 0
+      ? curry(f, left, combined)
+      : f.apply(null, combined);
+  };
 };
 
-const getNumber = () => new Promise((resolve) => {
-  setTimeout(() => resolve(10), 1000);
+const k = (v) => () => v;
+const compose = (...fs) => fs.reduce((a, b) => (...args) => a(b(...args)));
+const delay = (ms = 0) => new Promise((resolve) => setTimeout(() => resolve(), ms));
+const delayed = curry((ms, f) => delay(ms).then(_ => f()));
+const tap = curry((g, f) => (...args) => { g(...args); return f(...args); });
+const timeout = (ms) => delay(ms).then(_ => { throw new Timeout("timeout"); });
+const repeatedly = (n, f) => ((n <= 0) ? [] : [f(n), ...repeatedly(n-1, f)]);
+const doWhen = (pred, f) => () => pred() ? f() : undefined;
+
+const retryOnTimeout = curry((ms, n, req) => {
+  let done = false;
+  const delayedRequest = i => delayed((i-1)*ms, doWhen(() => !done, req));
+  return new Promise((resolve, reject) => {
+    const queue = [timeout(n*ms, req), ...repeatedly(n, delayedRequest)];
+    Promise.race(queue)
+      .then(resolve)
+      .catch(reject)
+      .finally(() => done = true);
+  });
 });
 
-reattempt(getNumber, ms = 250).then(console.log);
+const getMessage = (ms) => () => new Promise((resolve) => setTimeout(() => resolve("hello, world"), ms));
+const retryEverySecond = retryOnTimeout(500, 3);
+const withRetry = (req) => retryEverySecond(req).then(console.log).catch(console.log);
+
+
+withRetry(tap(() => console.log("called once"), getMessage(250)));
+withRetry(tap(() => console.log("called three, then timeout"), getMessage(4000)));
