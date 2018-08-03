9ea2dddb1d3382becbfd702fea15df8ade2fa06e
Emil Heineking <ebheineking@gmail.com>
2018-08-03 06:32:20 -0400

    cleaned up validation example

--- Blogs/LosTechies-Entity-Validation.js
+++ Blogs/LosTechies-Entity-Validation.js
@@ -3,55 +3,55 @@
  * https://lostechies.com/jimmybogard/2007/10/24/entity-validation-with-visitors-and-extension-methods/
  * Ported to JavaScript
  */
+{
+	class Order {
+		constructor(id, customer) {
+			this.id = id;
+			this.customer = customer;
+		}
 
-class Order {
-	constructor(id, customer) {
-		this.id = id;
-		this.customer = customer;
+		validate(out) {
+			throw Error("validate not implemented");
+		}
 	}
 
-	validate(out) {
-		out.errors = [...validator.brokenRules(this)];
-		return validator.isValid(this);
+	class OrderPersistenceValidator {
+		isValid(entity) {
+			return [...this.brokenRules(entity)].length > 0
+		}
+		*brokenRules(entity) {
+			if (entity.id < 0)
+				yield "id cannot be less than 0.";
+
+			if (entity.customer === "")
+				yield "must include a customer.";
+		}
 	}
-}
 
-class OrderPersistenceValidator {
-	isValid(entity) {
-		return [...this.brokenRules(entity)].length > 0
+	const Validator = {
+		validators: {},
+		registerFor(entity, validator) {
+			Validator.validators[entity.name] = validator;
+		}
 	}
-	*brokenRules(entity) {
-		if (entity.id < 0)
-			yield "id cannot be less than 0.";
 
-		if (entity.customer === "")
-			yield "must include a customer.";
+	Validator.registerFor(Order, new OrderPersistenceValidator());
+
+	const validationMixin = {
+		validate(out) {
+			const name = this.constructor.name;
+			const validator = Validator.validators[name];
+			out.errors = [...validator.brokenRules(this)];
+			return validator.isValid(this);
+		}
+	};
+
+	Object.assign(Order.prototype, validationMixin);
+
+	const order = new Order(-1, "");
+	const out = { errors: null };
+
+	if (order.validate(out)) {
+		console.log(JSON.stringify(out));
 	}
-}
-
-const Validator = {
-	validators: {},
-	registerFor(entity, validator) {
-		Validator.validators[entity.name] = validator;
-	}
-}
-
-Validator.registerFor(Order, new OrderPersistenceValidator());
-
-const validationMixin = {
-	validate(out) {
-		const name = this.constructor.name;
-		const validator = Validator.validators[name];
-		out.errors = [...validator.brokenRules(this)];
-		return validator.isValid(this);
-	}
-};
-
-Object.assign(Order.prototype, validationMixin);
-
-const order = new Order(-1, "");
-const out = { errors: null };
-
-if (order.validate(out)) {
-	console.log(JSON.stringify(out));
 }
