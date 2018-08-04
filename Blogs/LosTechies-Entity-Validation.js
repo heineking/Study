71239a8773c41462a1a53299d6a2d6976e0314e9
Emil Heineking <ebheineking@gmail.com>
2018-08-04 07:26:21 -0400

    added a functional example of entity validation to lostechies blog

--- Blogs/LosTechies-Entity-Validation.js
+++ Blogs/LosTechies-Entity-Validation.js
@@ -55,3 +55,74 @@
 		console.log(JSON.stringify(out));
 	}
 }
+
+/**
+ * 
+ */
+{
+
+	const curry = (f, arity = f.length, received = []) => {
+		return (...args) => {
+			const combined = [...received, ...args];
+			const argsLeft = arity - combined.length;
+			return argsLeft > 0
+				? curry(f, argsLeft, combined)
+				: f.apply(null, combined);
+		};
+	};
+
+	const makeValidator = (message, fn) => {
+		const v = (...args) => fn(...args);
+		v.message = message;
+		return v;
+	};
+
+	const isValid = (...validators) => (
+		(...args) => validators.reduce((errors, validator) =>
+			validator(...args) ? errors : [...errors, validator.message]
+		,[])
+	);
+
+	const entityValidator = curry((validator, object) => (
+		Object.keys(object).reduce((results, key) => ({
+			...results,
+			[key]: isValid(...(validator[key] || []))(object[key]),
+		}), {})
+	));
+
+	const not = (fn) => (...args) => !fn(...args);
+	const ofType = curry((type, x) => typeof x === type);	
+	const greaterThan = curry((n, x) => x > n);
+	const defined = (x) => x == null;
+	const empty = (x) => x === "";
+	const whiteSpace = (x) => x.replace(/\s/g, "") === "";
+	const nullOrEmpty = (x) => defined(x) || not(whiteSpace(""));
+
+	const isNumber = makeValidator("must be a number", ofType("number"));
+	const isString = makeValidator("must be a string", ofType("string"));	
+	const greaterThanZero = makeValidator("must be greater than zero", greaterThan(0));
+	const isNotNullOrEmpty = makeValidator("string cannot be null or empty", not(nullOrEmpty));
+
+	const orderValidator = entityValidator({
+		id: [isNumber, greaterThanZero],
+		customer: [isString, isNotNullOrEmpty]
+	});
+
+	const order = {
+		id: 0,
+		customer: ""
+	};
+
+	const validation = orderValidator(order);
+	console.log(JSON.stringify(validation, null, 2));
+	/*=>
+			{
+				"id": [
+					"must be greater than zero"
+				],
+				"customer": [
+					"string cannot be null or empty"
+				]
+			}
+	*/
+}
