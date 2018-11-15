f6024276a193ded83c7d5a48f47cdc2a292b9566
Emil Heineking <ebheineking@gmail.com>
2018-11-15 18:52:39 -0500

    added character frequency count

--- Huffman-Encoding/src/index.spec.ts
+++ Huffman-Encoding/src/index.spec.ts
@@ -1,7 +1,15 @@
 import { expect } from 'chai';
+import { getCharFrequencies } from './index';
 
 describe('test suite', () => {
   it('should have a working test suite', () => {
     expect(true).to.equal(true);
+  });
+});
+
+describe('getCharFrequencies()', () => {
+  it('should count character frequencies', () => {
+    const xs = 'foo'.split('');
+    expect(getCharFrequencies(xs)).to.eql({ f: 1, o: 2 });
   });
 });
--- Huffman-Encoding/src/index.ts
+++ Huffman-Encoding/src/index.ts
@@ -1,2 +1,17 @@
 // index.ts
+
+interface FrequencyTable {
+  [key: string]: number;
+}
+
+export const getCharFrequencies = (xs: string[]): FrequencyTable => {
+  return xs.reduce((freq: FrequencyTable, ch): FrequencyTable => {
+    if (!freq[ch]) {
+      freq[ch] = 0;
+    }
+    freq[ch] += 1;
+    return freq;
+  }, {});
+}
+
 console.log('hello, world');
