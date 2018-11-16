da791c861dfb043c35ad272dd48ab643573e24cf
Emil Heineking <ebheineking@gmail.com>
2018-11-15 21:02:00 -0500

    finished huffman encoding

--- Huffman-Encoding/src/index.spec.ts
+++ Huffman-Encoding/src/index.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { getCharFrequencies } from './index';
+import { getCharFrequencies, createTree, invertTree, encode } from './index';
 
 describe('test suite', () => {
   it('should have a working test suite', () => {
@@ -11,5 +11,26 @@ describe('getCharFrequencies()', () => {
   it('should count character frequencies', () => {
     const xs = 'foo'.split('');
     expect(getCharFrequencies(xs)).to.eql({ f: 1, o: 2 });
+  });
+});
+
+describe('createTree()', () => {
+  it('should create a tree from frequency table', () => {
+    const freq = getCharFrequencies('foo'.split(''));
+    expect(createTree(Object.entries(freq))).to.eql({'0': 'f', '1': 'o'});
+  });
+});
+
+describe('invertTree()', () => {
+  it('should invert the encoding tree', () => {
+    const freq = getCharFrequencies('foo'.split(''));
+    const tree = createTree(Object.entries(freq));
+    expect(invertTree(tree)).to.eql({ 'f': '0', 'o': '1' });
+  });
+});
+
+describe('encode()', () => {
+  it('should encode a string', () => {
+    expect(encode('foo')).to.equal('011');
   });
 });
--- Huffman-Encoding/src/index.ts
+++ Huffman-Encoding/src/index.ts
@@ -4,6 +4,11 @@ interface FrequencyTable {
   [key: string]: number;
 }
 
+interface Node {
+  0?: Node | string;
+  1?: Node | string;
+}
+
 export const getCharFrequencies = (xs: string[]): FrequencyTable => {
   return xs.reduce((freq: FrequencyTable, ch): FrequencyTable => {
     if (!freq[ch]) {
@@ -12,6 +17,33 @@ export const getCharFrequencies = (xs: string[]): FrequencyTable => {
     freq[ch] += 1;
     return freq;
   }, {});
-}
+};
 
-console.log('hello, world');
+export const createTree = (entries: Array<[string | Node, number]>): Node => {
+  const [x, y, ...rest] = entries.sort((a, b) => a[1] - b[1]);
+  if (y === undefined) {
+    return x[0] as Node;
+  } 
+  const node: Node = { "0": x[0], "1": y[0] };
+  const score: number = x[1] + y [1];
+  return createTree(rest.concat([[node, score]]));
+};
+
+export const invertTree = (x: any, path: string = ''): any => {
+  if (typeof x !== 'object') {
+    return { [x]: path };
+  }
+  let inverted = Object.create(null);
+  for(const [key, value] of Object.entries(x)) {
+    Object.assign(inverted, invertTree(value, `${path}${key}`));
+  }
+  return inverted;
+};
+
+export const encode = (str: string): string => {
+  const chars = str.split('');
+  const freq = getCharFrequencies(str.split(''));
+  const tree = createTree(Object.entries(freq));
+  const encoding = invertTree(tree);
+  return chars.map((char) => encoding[char]).join('');
+};
