092d772481bdcb4c1e40b71443b92ac724174286
Emil Heineking <ebheineking@gmail.com>
2020-07-03 08:21:01 -0400

    finished biased die exercise

--- Essential-Algorithms/code/02_numerical_algorithms.ts
+++ Essential-Algorithms/code/02_numerical_algorithms.ts
@@ -117,13 +117,56 @@ describe('4. Write an algorithm to use a biased six-sided die to generate fair v
   });
 
   it('should create a fair die out of a biased die', () => {
-    const counts = rollDie(die, 10000);
+    const counts = rollDie(die, 5000);
     const dists = Object.values(getDistributions(counts));
 
     const expected = 1 / 6;
 
     for (const dist of dists) {
-      expect(dist - expected).to.be.lessThan(0.01);
+      expect(dist - expected).to.be.lessThan(0.02);
     }
   });
+
+/*
+  Exercise 4, Part 2. How efficient is the biased to fair die algorithm?
+
+  First, let's work this out with a fair 6-sided die. The first roll we make
+  can be any number on the die, so that will be probability 1. The next roll
+  we can roll any side BUT the first one, which makes the probability of
+  a valid roll 5/6. Then next roll can be any side but the two sides from
+  before which makes probability 4/6.
+
+  Working this out further we get the following probability:
+
+    1 x 5/6 x 4/6 x 3/6 x 2/6 x 1/6 = 0.0154 or about 1.5%
+
+  The solutions in the book simplifies the above to 6! / 6^6 which is found
+  realizing that the fractions simplify to:
+
+    6 x 5 x 4 ... 1       6!
+    ----------------  =  ----- = 6! x (1/6)^6  =  0.0154
+    6 x 6 x 6 ... 6      6^6
+
+  In words, the probability of rolling all 6 sides of die in 6 tries is the
+  total number of permuntations of all 6 sides times the probability of the
+  combination of sides.
+
+  Now, we can use this to work out the probability of a biased die.
+
+  Assume that we have the following distribution:
+
+    1 = 0.3
+    2 = 0.3
+    3 = 0.1
+    4 = 0.1
+    5 = 0.1
+    6 = 0.1
+
+  The probability that we roll all the numbers in a row would be:
+
+    Number of permutations = 6!
+    Probability of each permutation = 0.3^2 x 0.1^5 = 0.00648
+
+    6! x 0.5 x 0.1^5 = 0.0036
+*/
 });
