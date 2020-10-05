66f571dfa8f12a5f9463c31708de399d40c47fac
Emil Heineking <ebheineking@gmail.com>
2020-10-05 06:38:40 -0400

    adds trapezoid rule and fn comments

--- Essential-Algorithms/code/02_numerical_algorithms/02_numerical_integration.ts
+++ Essential-Algorithms/code/02_numerical_algorithms/02_numerical_integration.ts
@@ -1,11 +1,31 @@
 import { expect, should } from 'chai';
-import { rectangleRule } from './integration';
+import { rectangleRule, trapezoidRule } from './integration';
 
-describe('rectangleRule', () => {
+describe('integration estimation', () => {
   const fn = (x: number) => 1 + x + Math.sin(2 * x);
-  it (`should estimate integratation for ${fn.toString()} within 1%`, () => {
-    const area = rectangleRule(fn, 0, 5, 100000);
-    const diff = Math.abs(area - 18.419);
-    expect(diff).to.be.lessThan(0.01);
+
+  const rules = [
+    {
+      name: 'rectangleRule',
+      fn: rectangleRule,
+      n: 100000,
+      error: 0.01,
+    },
+    {
+      name: 'trapezoidRule',
+      fn: trapezoidRule,
+      n: 100,
+      error: 0.01,
+    },
+  ];
+
+  rules.forEach((rule) => {
+
+    it(`should estimate area for ${fn.toString()} using ${rule.name} within error: ${rule.error}`, () => {
+      const area = rule.fn(fn, 0, 5, rule.n);
+      const diff = Math.abs(area - 18.419);
+      expect(diff).to.be.lessThan(rule.error);
+    });
+
   });
 });
--- Essential-Algorithms/code/02_numerical_algorithms/integration.ts
+++ Essential-Algorithms/code/02_numerical_algorithms/integration.ts
@@ -1,11 +1,47 @@
-export const rectangleRule = (fn: any, xmin: number, xmax: number, n: number): number => {
+type Fn = (x: number) => number;
+
+/**
+ * Estimates area under xmin and xmax using
+ * rectangles.
+ * @param fn Function that is being estimated
+ * @param xmin left side of area
+ * @param xmax right side of are 
+ * @param n number of rectangles to use to estimate area
+ */
+export const rectangleRule = (fn: Fn, xmin: number, xmax: number, n: number): number => {
+  // rectangle width
   const dx = (xmax - xmin) / n;
 
+  // add up areas
   let area = 0;
   let x = xmin;
 
   for (let i = 0; i < n; ++i) {
     area += (dx * fn(x));
+    x += dx;
+  }
+
+  return area;
+};
+
+/**
+ * Estimates area under function by using trapezoids.
+ * Also known as estimation by line segments
+ * @param fn Function that is being integrated
+ * @param xmin Left most side of area
+ * @param xmax Right most side of area
+ * @param n Number of trapezoids to use in estimation
+ */
+export const trapezoidRule = (fn: Fn, xmin: number, xmax: number, n: number): number => {
+  // trapezoid width
+  const dx = (xmax - xmin) / n;
+
+  // add up the areas
+  let area = 0;
+  let x = xmin;
+
+  for (let i = 0; i < n; ++i) {
+    area += dx * (fn(x) + fn(x + dx)) / 2;
     x += dx;
   }
 
