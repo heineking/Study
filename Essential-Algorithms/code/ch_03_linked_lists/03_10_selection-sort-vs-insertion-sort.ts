4b239840f977e37cc3f512bd6ec320966913f36f
Emil Heineking <ebheineking@gmail.com>
2020-11-02 06:06:48 -0500

    compares selectionSort and insertionSort

--- Essential-Algorithms/code/ch_03_linked_lists/03_10_selection-sort-vs-insertion-sort.ts
+++ Essential-Algorithms/code/ch_03_linked_lists/03_10_selection-sort-vs-insertion-sort.ts
@@ -1,6 +1,19 @@
 /*
-
+  Write a program similar to the one shown in Figure 3.14 that
+  compares the times needed by selectionsort and insertionsort to
+  sort a list of items. Which is faster?
 */
+
+/*
+  Selection sort is generally slower. Repeated runs shows that
+  insertion sort is about 10% faster than selection sort for
+  lists of length 10,000.
+
+  results for one run:
+    -insertion: 303.713392
+    -selection: 328.854202
+*/
+
 const basename = __filename.split('/').slice(-1)[0];
 import { expect } from 'chai';
 
@@ -16,7 +29,24 @@ const repeat = <T>(fn: () => T, n: number) => {
   return xs;
 };
 
+const timed = (fn: (...args: any[]) => any): (...args: any[]) => { result: any, executionTime: number } => {
+  return (...args: any[]): any => {
+    const start = process.hrtime();
+    const result = fn(...args);
+    const hrtime = process.hrtime(start);
+
+    const nanoseconds = (hrtime[0] * 1e9) + hrtime[1];
+    const milliseconds = nanoseconds / 1e6;
+
+    return {
+      result,
+      executionTime: milliseconds,
+    };
+  };
+};
+
 describe(basename, () => {
+
   const sorters = {
     selectionSort: selectionSort<number>((a, b) => a < b),
     insertionSort: insertionSort<number>((a, b) => a < b),
@@ -76,7 +106,25 @@ describe(basename, () => {
         const expected = sortArrayAsc(xs);
         expect(list.toArray()).to.eql(expected);
       });
-
     });
+  });
+
+  it('should compare algorithms', () => {
+    // arrange
+    const list1 = createList<number>();
+    const list2 = createList<number>();
+
+    const xs = repeat(Math.random, 10000);
+    xs.forEach(list1.push);
+    xs.forEach(list2.push);
+
+    // act
+    const selectionSortResult = timed(list1.sort)(selectionSort<number>((a, b) => a < b));
+    const insertionSortResult = timed(list2.sort)(insertionSort<number>((a, b) => a < b));
+
+    // assert
+    console.log(`\t-insertion: ${insertionSortResult.executionTime}`);
+    console.log(`\t-selection: ${selectionSortResult.executionTime}`);
+    expect(true).to.eql(true);
   });
 });
